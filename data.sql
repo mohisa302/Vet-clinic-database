fe8d1182a292337bf9227be2f26f60f9003bb4e9
mohi <mohisadeghi302@gmail.com>
2023-03-28 09:58:40 +0430

    Add new data

--- data.sql
+++ data.sql
@@ -1,7 +1,19 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES 
-('Gabumon', 'Nov 15, 2018', 8, 'true', 2),
-('Agumon', 'Feb 3, 2020', 10.23,'true', 0 ),
-('Pikachu', 'Jan 7, 2021', 15.04, 'false', 1),
-('Devimon', 'May 12, 2017', 11, 'true', 5);
+  ('Gabumon', 'Nov 15, 2018', 8, 'true', 2),
+  ('Agumon', 'Feb 3, 2020', 10.23,'true', 0 ),
+  ('Pikachu', 'Jan 7, 2021', 15.04, 'false', 1),
+  ('Devimon', 'May 12, 2017', 11, 'true', 5);
+
+ ALTER TABLE animals
+  ADD species varchar(100);
+
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES
+  ('Charmander', ' Feb 8, 2020', -11, 'false', 0),
+  ('Plantmon', ' Nov 15, 2021', -5.7, 'true', 2),
+  ('Squirtle', ' Apr 2, 1993', -12.13, 'false', 3),
+  ('Angemon', ' Jun 12, 2005', -45, 'true', 1),
+  ('Boarmon', 'Jun 7, 2005', 20.4, 'true', 7),
+  ('Blossom', ' Oct 13, 1998', 17, 'true', 3),
+  ('Ditto', 'May 14, 2022', 22, 'true', 4);
