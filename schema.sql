8c43009e39c59b91b3c3adef02a5951e334528e0
mohi <mohisadeghi302@gmail.com>
2023-03-28 15:32:45 +0430

    Add new tables

--- schema.sql
+++ schema.sql
@@ -11,3 +11,31 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
     ADD species varchar(100);
+
+CREATE TABLE owners (
+    id serial not null primary key,
+    full_name varchar(100),
+    age INT
+);    
+
+CREATE TABLE species (
+    id serial not null primary key,
+    name varchar(100)
+);    
+
+ALTER TABLE animals
+ADD species_id INT;
+
+
+ALTER TABLE animals
+ADD CONSTRAINT constraint_fk
+FOREIGN KEY (species_id)
+REFERENCES species(id);
+
+ALTER TABLE animals
+ADD owner_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT constraint_fk2
+FOREIGN KEY (owner_id)
+REFERENCES owners(id);
